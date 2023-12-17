fd4dbe369bbeb1712b13219ffa1897d51a686bad
aliaryu <aliaryu@yahoo.com>
2023-12-17 21:26:14 +0330

    dev : create basic user admin configuration

--- app_account/admin.py
+++ app_account/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+
+class UserAdmin(UserAdmin):
+
+    model = get_user_model()
+    search_fields = ["email"]
+    ordering = ["email"]
+    filter_horizontal = []
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'phone', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(get_user_model(), UserAdmin)
