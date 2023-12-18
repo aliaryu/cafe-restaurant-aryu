6929a94e30c8dc4a09c9e22b52e4cea646761c3d
aliaryu <aliaryu@yahoo.com>
2023-12-18 16:19:08 +0330

    dev : create staff admin

--- app_account/admin.py
+++ app_account/admin.py
@@ -5,6 +5,7 @@ from .models import User, Role, Staff
 
 class StaffInline(admin.StackedInline):
     model = Staff
+    verbose_name= "اطلاعات کارمند"
 
 
 @admin.register(User)
@@ -43,6 +44,26 @@ class RoleAdmin(admin.ModelAdmin):
     )
 
 
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    model         = Staff
+    list_display  = ("user", "role", "user_first_name", "user_last_name", "user_salary")
+    fieldsets     = (
+        ("اطلاعات کارمند", {"fields": ("user", "role")}),
+    )
+    add_fieldsets = (
+        ("اطلاعات مسئولیت", {"fields": ("user", "role")}),
+    )
 
+    def user_first_name(self, obj):
+        return obj.user.first_name
 
-admin.site.register(Staff)
+    def user_last_name(self, obj):
+        return obj.user.last_name
+    
+    def user_salary(self, obj):
+        return obj.role.salary
+
+    user_first_name.short_description = "نام"
+    user_last_name.short_description  = "نام خانوادگی"
+    user_salary.short_description     = "حقوق"
