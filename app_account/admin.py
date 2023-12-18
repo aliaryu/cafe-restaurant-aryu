3050c905e5ff902dcac3cfaa868dce00a43adb15
aliaryu <aliaryu@yahoo.com>
2023-12-18 09:31:45 +0330

    test : add role & staff to admin panel for testing

--- app_account/admin.py
+++ app_account/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
+from .models import Role, Staff
 
 
 class UserAdmin(UserAdmin):
@@ -23,3 +24,6 @@ class UserAdmin(UserAdmin):
     )
 
 admin.site.register(get_user_model(), UserAdmin)
+
+admin.site.register(Role)
+admin.site.register(Staff)
