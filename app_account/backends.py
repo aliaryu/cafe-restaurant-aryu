5b17d20e3c6d2a0b204bb992b2fae17c0be64671
aliaryu <aliaryu@yahoo.com>
2023-12-18 15:13:40 +0330

    fix : use try except block to avoid query error

--- app_account/backends.py
+++ app_account/backends.py
@@ -5,8 +5,9 @@ from django.db.models import Q
 
 class EmailOrPhoneModelBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-        user = get_user_model().objects.get(Q(email=username) | Q(phone=username))
-
-        if user and user.check_password(password):
-            return user
-        return None
+        try:
+            user = get_user_model().objects.get(Q(email=username) | Q(phone=username))
+            if user and user.check_password(password):
+                return user
+        except:
+            return None
