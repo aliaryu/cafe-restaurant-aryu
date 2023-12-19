3457a74347561eadc8fc54f0b4a01d8f6991960f
aliaryu <aliaryu@yahoo.com>
2023-12-19 21:39:58 +0330

    dev : create order admin & its inline

--- app_order/admin.py
+++ app_order/admin.py
@@ -2,6 +2,36 @@ from django.contrib import admin
 from .models import Order, OrderItem
 
 
-admin.site.register(Order)
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    model           = Order
+    inlines         = (OrderItemInline,)
+    ordering        = ("-is_complete", "date_time",)
+    search_fields   = ("pk", "user__first_name", "user__last_name", "user__phone")
+    list_display    = ("__str__", "user", "get_user_fullname", "date_time", "in_process", "is_complete", "staff")
+    fieldsets       = (
+        ("اطلاعات مشتری", {"fields": ("user", "get_user_fullname", "get_user_phone", "get_user_address")}),
+        ("اطلاعات سفارش", {"fields": ("staff", "date_time", "in_process", "is_complete")}),
+    )
+    readonly_fields = ("date_time", "user", "get_user_fullname", "get_user_phone", "get_user_address")
+
+    def get_readonly_fields(self, request, obj=None):
+        if not obj:
+            return ["date_time", "get_user_fullname", "get_user_phone", "get_user_address"]
+        return super().get_readonly_fields(request, obj)
+
+
+
+
+
+
+
+
+
 
 admin.site.register(OrderItem)
