85a1e34d3cd4144cc6ed5d0f6cf82514f6dec3e8
aliaryu <aliaryu@yahoo.com>
2023-12-19 08:26:10 +0330

    dev : create category model

--- app_item/models.py
+++ app_item/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(
+        max_length   = 255,
+        verbose_name = "دسته بندی"
+    )
+    image = models.ImageField(
+        upload_to    = "category_images/",
+        verbose_name = "عکس"
+    )
+
+    class Meta:
+        verbose_name        = "دسته بندی"
+        verbose_name_plural = "دسته بندی ها"
+
+    def __str__(self):
+        return str(self.category_name)
+
+
+
+
+
