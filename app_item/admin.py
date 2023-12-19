63c4619f10f3b6b93478ffac8f4f8df04e92c600
aliaryu <aliaryu@yahoo.com>
2023-12-19 09:29:57 +0330

    dev : create category admin

--- app_item/admin.py
+++ app_item/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Category
+from django.utils.html import format_html
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model           = Category
+    list_display    = ("category_name", "display_image")
+    readonly_fields = ("display_image",)
+    fieldsets       = (
+        ("اطلاعات دسته بندی", {"fields": ("category_name",)}),
+        ("عکس دسته بندی", {"fields": ("display_image", "image",)}),
+    )
+    add_fieldsets   = (
+        ("اطلاعات دسته بندی", {"fields": ("category_name",)}),
+        ("عکس دسته بندی", {"fields": ("image",)}),
+    )
+
+    def display_image(self, obj):
+        return format_html('<img src="{}" width="50" height="50" style="background-color: #121212;"/>'.format(obj.image.url))
+
+    display_image.short_description = "تصویر موجود"
