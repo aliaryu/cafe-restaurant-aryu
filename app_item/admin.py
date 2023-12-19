0263129ab751f495690c4107abfdc9cb298692f2
aliaryu <aliaryu@yahoo.com>
2023-12-19 10:13:14 +0330

    dev : create item admin

--- app_item/admin.py
+++ app_item/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Item, ItemComment
 from django.utils.html import format_html
 
 
@@ -12,12 +12,30 @@ class CategoryAdmin(admin.ModelAdmin):
         ("اطلاعات دسته بندی", {"fields": ("category_name",)}),
         ("عکس دسته بندی", {"fields": ("display_image", "image",)}),
     )
-    add_fieldsets   = (
-        ("اطلاعات دسته بندی", {"fields": ("category_name",)}),
-        ("عکس دسته بندی", {"fields": ("image",)}),
+
+    def display_image(self, obj):
+        return format_html('<img src="{}" width="50" height="50" style="background-color: #121212;"/>'.format(obj.image.url))
+
+    display_image.short_description = "تصویر موجود"
+
+
+
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    model           = Item
+    list_display    = ("item_name", "price", "count", "display_image")
+    readonly_fields = ("display_image",)
+    fieldsets       = (
+        ("اطلاعات آیتم", {"fields": ("item_name", "price", "count", "description")}),
+        ("عکس دسته بندی", {"fields": ("display_image", "image",)}),
     )
 
     def display_image(self, obj):
         return format_html('<img src="{}" width="50" height="50" style="background-color: #121212;"/>'.format(obj.image.url))
 
     display_image.short_description = "تصویر موجود"
+
+
+admin.site.register(ItemComment)
