39fe53064068fa1259ee48129cda4d9746c2ffe0
aliaryu <aliaryu@yahoo.com>
2023-12-20 21:14:42 +0330

    dev : create url for login page

--- conf/urls.py
+++ conf/urls.py
@@ -7,6 +7,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path("", include("app_home.urls"))
+    path("",         include("app_home.urls")),
+    path("account/", include("app_account.urls")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
