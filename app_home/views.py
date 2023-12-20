95d82a8b6a90c258b6f8bb30c10935658b15de00
aliaryu <aliaryu@yahoo.com>
2023-12-20 16:16:16 +0330

    dev : config templates settings and start merging front with back

--- app_home/views.py
+++ app_home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = "app_home/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        return context
