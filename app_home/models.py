d92849c81dc551a77cc3c7cfecd208bcfb0083e9
aliaryu <aliaryu@yahoo.com>
2023-12-20 15:23:48 +0330

    dev : create receive message model

--- app_home/models.py
+++ app_home/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class ReceiveMessage(models.Model):
+    fullname = models.CharField(
+        max_length   = 255,
+        verbose_name = "فرستنده"
+    )
+    email = models.EmailField(
+        max_length   = 255,
+        verbose_name = "ایمیل",
+    )
+    message = models.TextField(
+        verbose_name = "پیام"
+    )
+    date_time = models.DateTimeField(
+        auto_now_add = True,
+        verbose_name = "تاریخ و زمان"
+    )
+
+    class Meta:
+        verbose_name        = "پیام"
+        verbose_name_plural = "پیام ها"
+
+    def __str__(self):
+        return str(self.fullname)
