2f9e96577740f8c657fa7f8fb55032bd4ada2fa6
aliaryu <aliaryu@yahoo.com>
2023-12-21 10:18:17 +0330

    dev : improve query, select related, prefetch & so on, im good..

--- app_item/views.py
+++ app_item/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic import DetailView, View
-from .models import Item
+from .models import Item, ItemComment
 from django.db.models import F
+from django.db.models import Prefetch
 
 
 class ItemDetailVeiw(DetailView):
@@ -10,8 +11,15 @@ class ItemDetailVeiw(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["item"] = Item.objects.select_related("category").prefetch_related("itemcomment_set").get(pk=self.kwargs.get("pk"))
+        context["item"] = Item.objects.select_related("category").prefetch_related(
+            Prefetch("itemcomment_set", queryset=ItemComment.objects.filter(approve=True).select_related("user"))
+            ).get(pk=self.kwargs.get("pk"))
         return context
+
+        # GOOD QUERY ;p
+        # Item.objects.select_related("category").prefetch_related(
+        #     Prefetch("itemcomment_set", queryset=ItemComment.objects.filter(approve=True).select_related("user"))
+        #     ).get(pk=self.kwargs.get("pk"))
 
 
 class ItemLikeView(View):
