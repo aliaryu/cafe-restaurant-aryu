0bd9a4a49b66660fc19dcc11cbc0cdab87a64e90
aliaryu <aliaryu@yahoo.com>
2023-12-21 13:36:14 +0330

    dev : create url for order page

--- app_item/urls.py
+++ app_item/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ItemDetailVeiw, ItemLikeView
+from .views import ItemDetailVeiw, ItemLikeView, OrderView
 
 
 app_name = "app_item"
@@ -7,4 +7,5 @@ app_name = "app_item"
 urlpatterns = [
     path("item/<int:pk>/",      ItemDetailVeiw.as_view(), name="item_detail_page"),
     path("item-like/<int:pk>/", ItemLikeView.as_view(),   name="item_like_url"),
+    path("order/",              OrderView.as_view(),      name="order_page"),
 ]
