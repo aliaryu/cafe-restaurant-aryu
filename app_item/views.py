3c4492494557e47a5b9781850d31e7ac752c4eb3
aliaryu <aliaryu@yahoo.com>
2023-12-21 09:21:36 +0330

    dev : create item detail view

--- app_item/views.py
+++ app_item/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Item
 
-# Create your views here.
+
+class ItemDetailVeiw(DetailView):
+    model               = Item
+    template_name       = "app_item/item_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["item"] = Item.objects.select_related("category").prefetch_related("itemcomment_set").get(pk=self.kwargs.get("pk"))
+        return context
