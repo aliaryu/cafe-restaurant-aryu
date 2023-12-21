0ccc39a39ed4a6474f4ec72cf2977f11423e647e
aliaryu <aliaryu@yahoo.com>
2023-12-21 08:36:40 +0330

    dev : create receive message admin

--- app_home/admin.py
+++ app_home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import ReceiveMessage
 
-# Register your models here.
+
+@admin.register(ReceiveMessage)
+class UserAdmin(admin.ModelAdmin):
+    model         = ReceiveMessage
+    ordering      = ("-date_time",)
+    list_display  = ("fullname", "email", "date_time", "read")
+    fieldsets     = (
+        ("اطلاعات فرستنده", {"fields": ("fullname", "email")}),
+        ("اطلاعات پیام",    {"fields": ("message", "date_time")}),
+        ("وضعیت خواندن",   {"fields": ("read", )}),
+    )
