4b1d0eb354272444074a43d09d1a1bad2f37bb46
aliaryu <aliaryu@yahoo.com>
2023-12-21 09:39:08 +0330

    dev : create url & template item detail

--- app_item/urls.py
+++ app_item/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ItemDetailVeiw
+from .views import ItemDetailVeiw, ItemLikeView
 
 
 app_name = "app_item"
 
 urlpatterns = [
-    path("item/<int:pk>/", ItemDetailVeiw.as_view(), name="item_detail_page")
+    path("item/<int:pk>/",      ItemDetailVeiw.as_view(), name="item_detail_page"),
+    path("item-like/<int:pk>/", ItemLikeView.as_view(),   name="item_like_url"),
 ]
