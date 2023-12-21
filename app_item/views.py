49c88642a81c35d6d3e172d52cb03788acf0025e
aliaryu <aliaryu@yahoo.com>
2023-12-21 09:38:16 +0330

    dev : create item like view

--- app_item/views.py
+++ app_item/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import DetailView, View
 from .models import Item
+from django.db.models import F
 
 
 class ItemDetailVeiw(DetailView):
@@ -11,3 +12,11 @@ class ItemDetailVeiw(DetailView):
         context = super().get_context_data(**kwargs)
         context["item"] = Item.objects.select_related("category").prefetch_related("itemcomment_set").get(pk=self.kwargs.get("pk"))
         return context
+
+
+class ItemLikeView(View):
+    def get(self, request, pk):
+        item = Item.objects.select_related("category").prefetch_related("itemcomment_set").get(pk=self.kwargs.get("pk"))
+        item.like = F("like") + 1
+        item.save()
+        return redirect("app_item:item_detail_page", pk=pk)
