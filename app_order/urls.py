db0e726c5e231f547dbb9696655534213d12f912
aliaryu <aliaryu@yahoo.com>
2023-12-22 08:26:52 +0330

    rebase : full change payment system

--- app_order/urls.py
+++ app_order/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import CartView, PaymentView
+from .views import CartView
 
 
 app_name = "app_order"
 
 urlpatterns = [
     path("",         CartView.as_view(), name="cart_page"),
-    path("payment/", PaymentView.as_view(), name="payment_url")
 ]
--- app_order/views.py
+++ app_order/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, View
-from django.contrib.auth.mixins import LoginRequiredMixin
 from app_item.models import Item
+from .models import Order, OrderItem
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 import json
 
-
+@method_decorator(login_required, name='post')
 class CartView(ListView):
     template_name       = "app_order/cart.html"
     context_object_name = "items"
@@ -25,15 +27,19 @@ class CartView(ListView):
         context["items"] = output
         return context
 
-
-class PaymentView(LoginRequiredMixin, View):
-    def get(self, request):
+    def post(self, request):
         cookies = self.request.COOKIES.get("cart")
         cookies = json.loads(cookies) if isinstance(cookies, str) else {}
 
         if cookies:
-            
-            return redirect("app_home:home_page")
+            order = Order(user=request.user)
+            order.save()
+            items  = Item.objects.filter(id__in=list(cookies.keys()))
+            for key, value in cookies.items():
+                OrderItem.objects.create(order=order, item=items.get(id=int(key)), count=int(value))
+            response = redirect("app_home:home_page")
+            response.delete_cookie("cart")
+            return response
         else:
             return redirect("app_item:order_page")
 
