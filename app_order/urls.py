cc0e56395ad5977606c24c9b361199c047159983
aliaryu <aliaryu@yahoo.com>
2023-12-22 07:41:07 +0330

    dev : create url for payment

--- app_order/urls.py
+++ app_order/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CartView
+from .views import CartView, PaymentView
 
 
 app_name = "app_order"
 
 urlpatterns = [
-    path("", CartView.as_view(), name="cart_page")
+    path("",         CartView.as_view(), name="cart_page"),
+    path("payment/", PaymentView.as_view(), name="payment_url")
 ]
