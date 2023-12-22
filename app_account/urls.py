0b0a99aab7aeb9e7604a510dcc75832b50ac751e
aliaryu <aliaryu@yahoo.com>
2023-12-22 08:29:14 +0330

    dev : create profile url

--- app_account/urls.py
+++ app_account/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import LoginView, SignUpView, LogoutView
+from .views import LoginView, SignUpView, LogoutView, ProfileView
 
 
 app_name = "app_account"
 
 urlpatterns = [
-    path("login/", LoginView.as_view(), name="login_page"),
-    path("signup/", SignUpView.as_view(), name="signup_page"),
-    path("logout/", LogoutView.as_view(), name="logout_page"),
+    path("login/",   LoginView.as_view(),   name="login_page"),
+    path("signup/",  SignUpView.as_view(),  name="signup_page"),
+    path("logout/",  LogoutView.as_view(),  name="logout_page"),
+    path("profile/", ProfileView.as_view(), name="profile_page")
 ]
